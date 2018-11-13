3f5963ea60471796df48eec2442dde0e87002ce5
shwet04 <talktoshweta0@gmail.com>
2018-11-13 12:35:39 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -2,28 +2,21 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import pf from "petfinder-client";
-import Pet from "./Pet";
-
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
+import Results from "./Results";
 
 class App extends React.Component {
-
-  componentDidMount() {
-    const promise = petfinder.breed.list({ animal: "dog" });
-
-    promise.then(console.log, console.error);
-  }
   render() {
     return (
       <div>
-        <h1>Adopt Me!</h1>
-        <Pet name="Luna" animal="dog" breed="Havanese" />
-        <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-        <Pet name="Doink" animal="cat" breed="Mix" />
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
